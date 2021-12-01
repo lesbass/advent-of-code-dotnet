fc8fb7a61d9190a498bc1ae6cc0442a711b3b319
Stefano Maffeis <stefano.maffeis@gmail.com>
2021-12-01 22:44:15 +0100

    Initial commit

---
+++ src/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Text;
+
+namespace advent_of_code_dotnet
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Reads lines from the given input txt file.
+        /// </summary>
+        public static string[] ReadInput(string name) => File.ReadAllLines(Path.Combine( "inputs", $"{name}.txt"), Encoding.UTF8);
+    }
+}
